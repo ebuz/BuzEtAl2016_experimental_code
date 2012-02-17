758491ff94c5d94c6a8b94f0a35beeb5c41deafd
Andrew Watts <awatts@bcs.rochester.edu>
2012-02-17 13:20:24 -0500

    Partial progress on using new survey

--- socalign1.js
+++ socalign1.js
@@ -84,21 +84,50 @@
 
                 function validateSurvey() {
                     var formvalid = true;
-                    $.each(['feeling', 'exercise', 'politics', 'sports',
-                    'moodchange', 'moviefreq', 'pleasureread', 'cheerful',
-                    'currentevents', 'outgoing', 'similarpersonality',
-                    'likespeaker', 'speakersuccess', 'malefemale'], function(index, value) {
+                    $.each(['q.speaker.conservative','q.speaker.liberal',
+                            'q.speaker.articulate','q.speaker.accented',
+                            'q.speaker.intelligent','q.speaker.educated',
+                            'q.speaker.self-centered','q.speaker.generous',
+                            'q.speaker.weak_arguments','q.speaker.shy',
+                            'q.speaker.enthusiastic','q.speaker.easy_to_understand',
+                            'q.participant.speaker_speaks_like_me',
+                            'q.participant.speaker_is_similar_to_me',
+                            'q.participant.speaker_would_understand_me',
+                            'q.participant.agree_with_speaker',
+                            'q.participant.want_speaker_as_friend','q.participant.education',
+                            'q.ideology.conservative','q.ideology.liberal',
+                            'q.ideology.republicans','q.ideology.democrats',
+                            'q.ideology.enjoy_accents','q.ideology.proper_english',
+                            'q.ideology.official_language',
+                            'q.ideology.importance_of_speaking_well',
+                            'q.ideology.accent_and_self-presentation',
+                            'q.ideology.accent_predicts_intelligence',
+                            'q.conflict.dominate.my_way_best','q.conflict.avoid.ignore',
+                            'q.conflict.integrate.meet_halfway',
+                            'q.conflict.dominate.insist_my_position_be_accepted',
+                            'q.conflict.avoid.pretend_nothing_happend',
+                            'q.conflict.avoid.pretend_no_conflict',
+                            'q.conflict.integrate.middle_course',
+                            'q.conflict.dominate.dominate_until_other_understands',
+                            'q.conflict.integrate.give_and_take']
+, function(index, value) {
                         if ($('[name="' + value + '"]:checked')[0] === undefined) {
                             $('#'+ value).css('color', 'red');
                             formvalid = false;
                         }
                     });
-                    $.each(['speakerage', 'sleep'], function(index, value) {
-                        if ($('[name="' + value + '"]').val() === 'NA') {
-                            $('#'+ value).css('color', 'red');
-                            formvalid = false;
-                        }
-                    });
+
+                    if($('[name="q.participant.gender"]:checked')[0] === undefined &&
+                       $('[name="q.participant.gender.other"]').val() == '') {
+                        $('#q.participant.gender').css('color', 'red');
+                        $('#q.participant.gender.other').css('color', 'red');
+                        formvalid = false;
+                    }
+
+                    if($('[name="q.participant.age"]').val() === '') {
+                        $('#q.participant.age').css('color', 'red');
+                        formvalid = false;
+                    }
                     return formvalid;
                 }
 
